209e213c3ad9c880eb381c05a79ebe80e58b89f7
Vito-M <v.marchionna@studenti.uniba.it>
2026-06-06 16:40:43 +0200

    Fix: HTTP to HTTPS

--- assets/js/ui-notifications.js
+++ assets/js/ui-notifications.js
@@ -6,7 +6,7 @@ var UINotifications = function() {"use strict";
 		var $toastlast;
 
 		var getMessage = function() {
-			var msgs = ['My name is Inigo Montoya. You killed my father. Prepare to die!', '<div><input class="input-small" value="textbox"/>&nbsp;<a href="http://johnpapa.net" target="_blank">This is a hyperlink</a></div><div><button type="button" id="okBtn" class="btn btn-primary">Close me</button><button type="button" id="surpriseBtn" class="btn" style="margin: 0 8px 0 8px">Surprise me</button></div>', 'Are you the six fingered man?', 'Inconceivable!', 'I do not think that means what you think it means.', 'Have fun storming the castle!'];
+			var msgs = ['My name is Inigo Montoya. You killed my father. Prepare to die!', '<div><input class="input-small" value="textbox"/>&nbsp;<a href="https://johnpapa.net" target="_blank">This is a hyperlink</a></div><div><button type="button" id="okBtn" class="btn btn-primary">Close me</button><button type="button" id="surpriseBtn" class="btn" style="margin: 0 8px 0 8px">Surprise me</button></div>', 'Are you the six fingered man?', 'Inconceivable!', 'I do not think that means what you think it means.', 'Have fun storming the castle!'];
 			i++;
 			if(i === msgs.length) {
 				i = 0;
